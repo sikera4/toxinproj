dcf689ceee4803e863e72d0487b6eea9ecb1223b
Andrew <sikera0812@mail.ru>
2021-04-10 00:47:41 +0300

    start form elements do first two textfields, dropdown, masked textfield, two date-dropdowns, filter date dropdown and subscription field connecnt jquery plugin for input mask (inputmask (lol))

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import '../pages/colors_type/colors_type.scss';
 import '../pages/cards/cards.scss';
+import '../pages/form_elements/form_elements.scss'
 import '../node_modules/air-datepicker/dist/css/datepicker.min.css';
 import '../node_modules/air-datepicker/dist/js/datepicker.min.js';
 import '../node_modules/jquery-nice-select/js/jquery.nice-select.js';
 import '../node_modules/jquery-nice-select/css/nice-select.css';
+import '../node_modules/inputmask/dist/jquery.inputmask.js'
 
-$(document).ready(function() {
+$(function() {
     $('select').niceSelect();
   });
+
+$(function(){
+    $('#masked').inputmask("99.99.9999", { "placeholder": "ДД.ММ.ГГГГ" });
+});
