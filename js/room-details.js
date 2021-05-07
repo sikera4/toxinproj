cbe0dbd611f5b3390993ee95787dc6540df4a6b5
Andrew <sikera0812@mail.ru>
2021-05-07 15:38:47 +0300

    - do the whole room details page except for minor modifications for the card in it; - modify commentary scss, add a mixin for person's photo; - download and configurate chart.js;

--- js/room-details.js
+++ js/room-details.js
@@ -1,8 +1,62 @@
 import {finalCounterCard} from '../common.blocks/final-counter-card/final-counter-card.js';
+import Chart from 'chart.js/auto';
+
+function chartConstruct() {
+    let ctx = document.getElementsByClassName('js-reviews-chart')[0].getContext("2d");
+
+    let gradientPurple = ctx.createLinearGradient(0, 0, 0, 120);
+    gradientPurple.addColorStop(0, '#BC9CFF');
+    gradientPurple.addColorStop(1, '#8BA4F9');
+
+    let gradientGreen = ctx.createLinearGradient(0, 0, 0, 120);
+    gradientGreen.addColorStop(0, '#6FCF97');
+    gradientGreen.addColorStop(1, '#66D2EA');
+
+    let gradientGolden = ctx.createLinearGradient(0, 0, 0, 120);
+    gradientGolden.addColorStop(0, '#FFE39C');
+    gradientGolden.addColorStop(1, '#FFBA9C');
+
+    let chartData = {
+        labels: [
+            'Удовлетворительно',
+            'Хорошо',
+            'Великолепно',
+            'Разочарован'
+        ],
+        datasets: [{
+            label: 'My First Dataset',
+            data: [ 65, 65, 130, 0],
+            backgroundColor: [
+                gradientPurple,
+                gradientGreen,
+                gradientGolden,
+                '#919191'
+            ],
+            hoverOffset: 4,
+            cutout: '90%'
+        }]
+    }
+
+    let myChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: chartData,
+        options: {
+            plugins: {
+                legend: {
+                    display: false
+                },
+                tooltip: {
+                    enabled: false
+                }
+            },
+            
+        }
+    })
+}
 
 $(function() {
     finalCounterCard();
+    chartConstruct();
 })
-
 
 import '../pages/room_details/room-details.scss';
