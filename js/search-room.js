d0f5ca80bf59564459bdfb03b745ae76533446ee
Andrew <sikera0812@mail.ru>
2021-05-22 20:30:21 +0300

    - responsive layout for landing - responsive layout for search-room page - add footer tag for footers on landing and search-page

--- js/search-room.js
+++ js/search-room.js
@@ -1,12 +1,22 @@
 import { iqDropdown } from '../common.blocks/quantity-dropdown/quantity-dropdown';
 import { checkList } from '../common.blocks/expandable-checkbox-list/expandable-checkbox-list';
 import { sliderActivation } from '../common.blocks/range-slider/range-slider';
+import { menuBtn } from '../common.blocks/header/header';
+function filter() {
+    let filterHeader = $('.search-room__filter-header');
+    let filterSettings = $('.search-room__filter-settings');
+    filterHeader.on('click', function() {
+        filterSettings.toggleClass('open');
+    })
+}
 
 $(function() {
     checkList();
     iqDropdown('guests-info');
     iqDropdown('room-type');
     sliderActivation();
+    filter();
+    menuBtn();
 })
 
 
